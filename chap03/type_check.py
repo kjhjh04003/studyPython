52c657827cbb531db24c5e3887ee7be4281ac4ff
kjhjh <kjhjh04003@naver.com>
2021-11-03 00:36:01 +0900

    chap03 숫자 확인 문제

---
+++ chap03/type_check.py
@@ -0,0 +1,17 @@
+# 1시간은 몇 초인가?
+print(60*60)
+
+# 계산한 결과를 seconds_per_hour 변수에 저장
+seconds_per_hour = 60*60
+
+# 1일은 몇 초인가? seconds_per_hour 변수 사용
+print(seconds_per_hour*24)
+
+# 계산한 결과를 seconds_per_day 변수에 저장
+seconds_per_day = seconds_per_hour*24
+
+# 부동 소수점(/) 나눗셈을 사용해서 seconds_per_day를 seconds_per_hour로 나누어 보자
+print(seconds_per_day/seconds_per_hour)
+
+# 정수(//)나눗셈을 사용해서 seconds_per_day를 seconds_per_hour로 나눈다.
+print(seconds_per_day//seconds_per_hour)
