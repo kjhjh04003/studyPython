7717e7535e8be194e410397ad3a91cfb02087087
kjhjh <kjhjh04003@naver.com>
2021-11-03 00:35:26 +0900

    chap02 데이터 확인문제

---
+++ chap02/type_basic_check.py
@@ -0,0 +1,12 @@
+# 정수 99를 변수 prince에 할당하고 출력
+prince = 99
+print(prince)
+
+# 값5는 어떤 타입인가 ? int
+print(type(5))
+
+# 값 2.0은 어떤 타입인가? float
+print(type(2.0))
+
+# 표현식 5+2.0은 어떤 타입인가? float
+print(type(5+2.0))
